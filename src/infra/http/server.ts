8d7a9622693949fa11ec8eae5c30090c73673987
Aurino Geraldo <aurinoaj@gmail.com>
2025-04-01 22:13:43 -0300

    feat: configure swagger

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,10 +1,15 @@
 import { fastifyCors } from "@fastify/cors"
+import fastifyMultipart from "@fastify/multipart"
+import fastifySwagger from "@fastify/swagger"
+import { fastifySwaggerUi } from "@fastify/swagger-ui"
 import { fastify } from "fastify"
 import {
   hasZodFastifySchemaValidationErrors,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from "fastify-type-provider-zod"
+import { uploadImageRoute } from "./routes/upload-images"
 
 const server = fastify()
 
@@ -30,6 +35,22 @@ server.setErrorHandler((error, _, reply) => {
 })
 
 server.register(fastifyCors, { origin: "*" })
+
+server.register(fastifyMultipart)
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Upload Server",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+})
+server.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+})
+
+server.register(uploadImageRoute)
 
 server.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("HTTP server running!")
