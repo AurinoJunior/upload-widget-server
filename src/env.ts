74524a575e7359f8b39af36a064bd5a0c2d31c8a
Aurino Geraldo <aurinoaj@gmail.com>
2025-04-14 18:11:50 -0300

    configurando instancia do r2 com sdk client-s3

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,11 @@ const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
   NODE_ENV: z.enum(["development", "production", "test"]).default("production"),
   DATABASE_URL: z.string().url().startsWith("postgresql://"),
+  CLOUFLARE_ACCOUNT_ID: z.string().min(1),
+  CLOUDFLARE_ACCESS_KEY_ID: z.string().min(1),
+  CLOUDFLARE_SECRET_ACCESS_KEY: z.string().min(1),
+  CLOUDFLARE_BUCKET: z.string().min(1),
+  CLOUDFLARE_PUBLIC_URL: z.string().min(1),
 })
 
 export const env = envSchema.parse(process.env)
