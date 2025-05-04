88cfc65900354e3b3c942a7aff50aa20b0ab8e43
Aurino Geraldo <aurinoaj@gmail.com>
2025-05-04 19:08:09 -0300

    Criando script de para rodar testes

---
+++ scripts/run-tests.js
@@ -0,0 +1,26 @@
+const { spawn } = require("node:child_process");
+
+// Executa de forma sequencial
+const commandsToRunTests = [
+  "yarn services:up",
+  "node scripts/wait-for-db.js",
+  "node scripts/reset-db.js",
+  "yarn db:migrate:test",
+  "dotenv -e .env.test -- vitest run",
+].join(" && ");
+
+spawn(commandsToRunTests, { stdio: "inherit", shell: true });
+
+function servicesStop() {
+  console.warn("Encerrando...");
+
+  spawn("yarn services:stop", {
+    detached: true, // continua executando mesmo depois de encerrar o processo principal.
+    shell: true,
+    windowsHide: true,
+    stdio: "ignore",
+  });
+}
+
+process.on("SIGINT", servicesStop); // SIGINT -> Saida forçada Ex: ctrl+c
+process.on("SIGTERM", servicesStop); // SIGTERM -> saida natural
