e245c71dab4b22b3e64194aade44c133025e1530
Aurino Junior <aurinoaj@gmail.com>
2025-06-09 20:15:36 -0300

    Adicionando novos comentarios

--- src/infra/http/routes/transform-swagger-schema.ts
+++ src/infra/http/routes/transform-swagger-schema.ts
@@ -2,8 +2,11 @@ import { jsonSchemaTransform } from "fastify-type-provider-zod"
 
 type TransformSwaggerSchemaData = Parameters<typeof jsonSchemaTransform>[0]
 
+// Adiciona suporte para o multipart/form-data
 export function transformSwaggerSchema(data: TransformSwaggerSchemaData) {
   const { schema, url } = jsonSchemaTransform(data)
+
+  // Verifica se o schema tem o tipo multipart/form-data
   if (!schema.consumes?.includes("multipart/form-data")) {
     return { schema, url }
   }
@@ -21,6 +24,7 @@ export function transformSwaggerSchema(data: TransformSwaggerSchemaData) {
     format: "binary",
   }
 
+  // Adiciona o campo file como obrigatório
   schema.body.required.push("file")
 
   return { schema, url }
